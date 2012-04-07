bd1a4d18324cb2041a2555a08bc22801f3c92c8a
Paul Bartrum <paulbartrum@hotmail.com>
2012-04-07 17:50:10 +1200

    Migration fix.

--- Backend/Migrations/10.sql
+++ Backend/Migrations/10.sql
@@ -1,2 +1,7 @@
 ﻿alter table [User]
-alter column EmailAddress nvarchar(256) not null
+drop constraint UK_User_EmailAddress;
+
+alter table [User]
+alter column EmailAddress nvarchar(256) not null;
+
+CREATE UNIQUE NONCLUSTERED INDEX UK_User_EmailAddress ON dbo.[User] (EmailAddress);
