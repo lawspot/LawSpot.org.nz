b3ff1b07fee0ef556604ebecf3ac6435dbf06105
Paul Bartrum <paulbartrum@hotmail.com>
2012-04-15 20:58:36 +1200

    Fix up the blanking of the header.

--- Shared/Layout/Layout.js
+++ Shared/Layout/Layout.js
@@ -1,5 +1,18 @@
 ﻿/* {{Include mustache.js}} */
 
+/* Tabs */
+if (Model.AskALawyerTabActive)
+    document.getElementById("_askALawyerTab").className = "active";
+if (Model.BrowseAnswersTabActive)
+    document.getElementById("_browseAnswersTab").className = "active";
+if (Model.HowItWorksTabActive)
+    document.getElementById("_howItWorksTab").className = "active";
+
+/* Login bar */
+if (Model.User !== null) {
+    document.getElementById("_notLoggedIn").style.display = "none";
+    document.getElementById("_loggedIn").style.display = "block";
+}
+
 document.getElementById("_content").innerHTML = Mustache.render(document.getElementById("_contentTemplate").text, Model);
-document.getElementById("_header").innerHTML = Mustache.render(document.getElementById("_headerTemplate").text, Model);
 document.getElementById("_message").innerHTML = Mustache.render(document.getElementById("_messageTemplate").text, Model);
