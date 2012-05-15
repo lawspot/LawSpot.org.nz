c091f3c9b3d9f96f448f985ef0f72948aba238d5
Paul Bartrum <paulbartrum@hotmail.com>
2012-05-15 23:54:03 +1200

    Fix javascript issue with IE 8.

--- Views/Ask/Ask/Ask.js
+++ Views/Ask/Ask/Ask.js
@@ -11,28 +11,33 @@ function countCharacters(textElement, charCountElement, limit) {
 countCharacters(document.getElementById("Title"), document.getElementById("TitleCharCount"), 150);
 countCharacters(document.getElementById("Details"), document.getElementById("DetailsCharCount"), 600);
 
-// Stretch the divider to the full height of the form.
 function stretchDivider() {
     var height = document.getElementById("qsubmit1").offsetHeight;
     document.getElementById("qsubmit-overlay2").style.borderTop = (height / 2) + "px solid transparent";
     document.getElementById("qsubmit-overlay2").style.borderBottom = (height / 2) + "px solid transparent";
 }
-stretchDivider();
 
-document.getElementById("login-link").onclick = function () {
-    document.getElementById("register").style.display = "none";
-    document.getElementById("login").style.display = "block";
-    stretchDivider();
-    document.getElementById("ShowRegistration").value = "false";
-    document.getElementById("LoginEmailAddress").focus();
-    return false;
-};
+// Stretch the divider to the full height of the form.
+if (document.getElementById("qsubmit-overlay2")) {
 
-document.getElementById("register-link").onclick = function () {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("register").style.display = "block";
     stretchDivider();
-    document.getElementById("ShowRegistration").value = "true";
-    document.getElementById("EmailAddress").focus();
-    return false;
-};
+
+    document.getElementById("login-link").onclick = function () {
+        document.getElementById("register").style.display = "none";
+        document.getElementById("login").style.display = "block";
+        stretchDivider();
+        document.getElementById("ShowRegistration").value = "false";
+        document.getElementById("LoginEmailAddress").focus();
+        return false;
+    };
+
+    document.getElementById("register-link").onclick = function () {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("register").style.display = "block";
+        stretchDivider();
+        document.getElementById("ShowRegistration").value = "true";
+        document.getElementById("EmailAddress").focus();
+        return false;
+    };
+
+}
