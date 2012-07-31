822be5442939de535f8322b4feead2c63ab71253
Paul Bartrum <paulbartrum@hotmail.com>
2012-07-31 23:47:57 +1200

    New categories.

---
+++ Backend/Migrations/36.sql
@@ -0,0 +1,7 @@
+﻿delete Category where Name = 'Tax Law'
+delete Category where Name = 'Building Law'
+insert Category (Name, Slug, ApprovedQuestionCount) values ('Maori Legal Issues', 'maori-legal', 0)
+insert Category (Name, Slug, ApprovedQuestionCount) values ('Youth Law', 'youth-law', 0)
+insert Category (Name, Slug, ApprovedQuestionCount) values ('Education', 'education', 0)
+insert Category (Name, Slug, ApprovedQuestionCount) values ('Driving and Traffic', 'driving-traffic', 0)
+update Category set Name = 'Health, Disability and ACC' where Name = 'Health and ACC'
